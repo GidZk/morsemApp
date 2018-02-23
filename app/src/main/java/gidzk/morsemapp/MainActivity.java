7929a5bb12891419295a9a9e7f93e23579f5a9d6
John <GidZk@MacTurbo.local>
2018-02-23 12:47:36 +0100

    Added startupActivity-got started on startupactivity

--- app/src/main/java/gidzk/morsemapp/MainActivity.java
+++ app/src/main/java/gidzk/morsemapp/MainActivity.java
@@ -1,13 +1,31 @@
 package gidzk.morsemapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b = findViewById(R.id.playButton);
+        b.setOnClickListener(this);
+
+    }
+
+
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this,InputActivity.class);
+        startActivity(intent);
+
+
     }
 }
